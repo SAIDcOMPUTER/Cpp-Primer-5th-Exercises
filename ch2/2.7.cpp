c6985e1ebee01c2dda5aa592029ac43d009afe9b
SAIDcOMPUTER <89578655+SAIDcOMPUTER@users.noreply.github.com>
2021-10-15 12:39:23 +0300

    Update 2.7.cpp

--- ch2/2.7.cpp
+++ ch2/2.7.cpp
@@ -1,5 +1,5 @@
 int main() {
-  "Who goes with F\145rgus?\012";  // character string literal containing two octal escape sequences
+  "Who goes with F\145rgus?\012";  // character string literal containing one decimal and one octal escape sequences
   3.14e1L;  // floating-point literal, type is `long double`
   1024f;  // floating-point literal, type is `float`
   3.14L;  // floating-point literal, type is `long double`
